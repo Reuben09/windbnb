07a975a9ab6c5763920dd6229b8807ee074ea718
Reuben09 <66923560+Reuben09@users.noreply.github.com>
2022-03-05 08:05:09 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ export default function App() {
   const [list, setList] = useState(Data);
 
   const handleList = (name) => {
-    let newItem = Data.filter((item) => item.city === name);
+     if(name === "Add Location"){
+      setList(Data);
+      return;
+    }
+    const newItem = Data.filter((item) => item.city === name);
     setList(newItem);
     console.log(name);
   };
